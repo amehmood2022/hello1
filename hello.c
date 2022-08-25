3d33dc7f34e5fff1e5ed965ab9e72b8c484ecfa5
Aryan Mehmood <amehmood2@huskers.unl.edu>
2022-08-25 20:39:24 +0000

    intial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+Author: Aryan Mehmood
+Date 2022/08/23
+
+Simple Hello World
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Hello World!\n");
+    printf("Aryan, Andrew\n");
+    return 0;
+}
